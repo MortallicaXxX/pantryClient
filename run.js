da532d4da0951b620b60e86c0407c18d0bd9e03e
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2021-06-23 12:41:20 +0200

    Update run.js

--- run.js
+++ run.js
@@ -1,26 +1,26 @@
 const pantryClient = (new pantry()).pantryClient;
-const client = new pantryClient("");
+const client = new pantryClient("pantryID");
 
 client.connect(async function(err) {
   console.log(`PantryClient est prêt.`);
   console.log(client);
 
-  client.basket('').content(function(err,result){
+  client.basket('basketName').content(function(err,result){
     if(err)throw err;
     else console.log(result);
   });
 
-  client.basket('').insert({test:"test"},function(err,result){
+  client.basket('basketName').insert({test:"test"},function(err,result){
     if(err)throw err;
     else console.log(result);
   });
 
-  client.basket('').update({test:"lol"},function(err,result){
+  client.basket('basketName').update({test:"lol"},function(err,result){
     if(err)throw err;
     else console.log(result);
   });
 
-  client.basket('').delete(function(err,result){
+  client.basket('basketName').delete(function(err,result){
     if(err)throw err;
     else console.log(result);
   });
