6491749c2e7d2ffadec303d2afc5d534008eff75
MortallicaXxX <MortallicaXxX@hotmail.be>
2021-06-23 02:29:33 +0200

    init

---
+++ run.js
@@ -0,0 +1,12 @@
+const pantryClient = (new pantry()).pantryClient;
+const client = new pantryClient("");
+
+client.connect(function(err) {
+  console.log(`PantryClient est prêt.`);
+  console.log(client);
+  // const db = client.db(dbName);
+  // client.close();
+  client.basket('').content(function(err,result){
+    console.log(err,result);
+  });
+});
